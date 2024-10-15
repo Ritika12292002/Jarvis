18e2df6e9701d82917c4ac7bb61e7995203e4cd3
Ritika Jindal <ritikajind292002@gmail.com>
2024-10-15 11:26:16 +0530

    first commit

---
+++ www/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+   
+
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bounceIn",
+        },
+        out: {
+            effect: "bounceOut",
+        },
+
+    });
+
+});
